2493bf43f17b74b50a6a4ddbc04068ac4a89aa77
rohitchauhan533 <chauhanrohit533@gmail.com>
2023-09-22 23:42:19 +0530

    card creation

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
+import Card from '@/components/card'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main>
+      <Card/>
       todo next js application
     </main>
   )
